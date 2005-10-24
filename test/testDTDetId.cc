2c0fe2523dcddbf25142b97de5b0a82abbd80d3d
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2005-10-24 15:56:19 +0000

    Cleanup: ctors, unsigned removed

--- test/testDTDetId.cc
+++ test/testDTDetId.cc
@@ -3,22 +3,22 @@
    test file for DTDetId
 
    \author Stefano ARGIRO
-   \version $Id: testDTDetId.cc,v 1.1 2005/08/02 15:46:33 argiro Exp $
+   \version $Id: testDTDetId.cc,v 1.2 2005/10/18 17:57:47 namapane Exp $
    \date 27 Jul 2005
 */
 
-static const char CVSId[] = "$Id: testDTDetId.cc,v 1.1 2005/08/02 15:46:33 argiro Exp $";
-
-
-#include <iostream>
-
 #include <cppunit/extensions/HelperMacros.h>
 #include <DataFormats/MuonDetId/interface/DTDetId.h>
+#include <FWCore/Utilities/interface/Exception.h>
+
+#include <iostream>
+using namespace std;
 
 class testDTDetId: public CppUnit::TestFixture
 {
 CPPUNIT_TEST_SUITE(testDTDetId);
 CPPUNIT_TEST(testOne);
+CPPUNIT_TEST(testFail);
 CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -26,7 +26,7 @@ public:
   void tearDown(){}
 
   void testOne();
-
+  void testFail();
 };
 
 ///registration of the test so that the runner can find it
@@ -34,28 +34,59 @@ CPPUNIT_TEST_SUITE_REGISTRATION(testDTDetId);
 
 void testDTDetId::testOne(){
 
-  for (int wheel=DTDetId::minWheelId; wheel<=DTDetId::maxWheelId   ; ++wheel)
-    for (unsigned int station=DTDetId::minStationId; 
+  for (int wheel=DTDetId::minWheelId; wheel<=DTDetId::maxWheelId; ++wheel)
+    for (int station=DTDetId::minStationId; 
 	 station <= DTDetId::maxStationId ; ++station)
-      for (unsigned int sector=DTDetId::minSectorId; 
+      for (int sector=DTDetId::minSectorId; 
 	   sector<=DTDetId::maxSectorId; ++sector)
-	for (unsigned int slayer=DTDetId::minSuperLayerId; 
+	for (int slayer=DTDetId::minSuperLayerId; 
 	     slayer<=DTDetId::maxSuperLayerId; ++slayer)
-	  for (unsigned int layer=DTDetId::minLayerId; 
+	  for (int layer=DTDetId::minLayerId; 
 	       layer<=DTDetId::maxLayerId; ++layer)
-	    for (unsigned int wire=DTDetId::minWireId; 
+	    for (int wire=DTDetId::minWireId; 
 		 wire<=DTDetId::maxWireId; ++wire){
-
+	      
 	      DTDetId detid(wheel, station, sector, slayer, layer, wire);
-
+	      
 	      CPPUNIT_ASSERT(detid.wheel() == wheel);
 	      CPPUNIT_ASSERT(detid.station() == station);
 	      CPPUNIT_ASSERT(detid.sector() == sector);
 	      CPPUNIT_ASSERT(detid.superlayer() == slayer);
 	      CPPUNIT_ASSERT(detid.layer() == layer);
 	      CPPUNIT_ASSERT(detid.wire() == wire);
+
+	      // test constructor from id
+	      int myId = detid.rawId();
+	      DTDetId anotherId(myId);
+	      CPPUNIT_ASSERT(detid==anotherId);
 	    }
 	    
 
+  
+}
 
+void testDTDetId::testFail(){
+  
+  // contruct using an invalid input index
+  try {
+    // Invalid layer
+    DTDetId detid(0,1,1,1,7,1);
+    CPPUNIT_ASSERT("Failed to throw required exception" == 0);      
+    detid.rawId(); // avoid compiler warning
+  } catch (cms::Exception& e) {
+    // OK
+  } catch (...) {
+    CPPUNIT_ASSERT("Threw wrong kind of exception" == 0);
+  }
+  
+  // contruct using an invalid input id
+  try {
+    DTDetId detid(3211);
+    CPPUNIT_ASSERT("Failed to throw required exception" == 0);      
+    detid.rawId(); // avoid compiler warning
+  } catch (cms::Exception& e) {
+    // OK
+  } catch (...) {
+    CPPUNIT_ASSERT("Threw wrong kind of exception" == 0);
+  }
 }
