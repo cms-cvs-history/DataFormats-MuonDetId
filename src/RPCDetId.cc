e80879344951e0cabdc2c1019da11ace478957cd
cmsbuild <>
2009-05-31 15:33:30 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- MuonDetId-V01-00-02'.
    Sprout from master 2008-11-06 10:34:56 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Remove <iostream> include from DTChamberId.h'
    Cherrypick from master 2009-05-31 15:32:35 UTC Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch> 'bug fix for detId loaded from DB':
        src/RPCDetId.cc
    Delete:
        test/cdan.cfg

--- src/RPCDetId.cc
+++ src/RPCDetId.cc
@@ -2,7 +2,7 @@
  * Impl of RPCDetId
  *
  * \author Ilaria Segoni
- * \version $Id: RPCDetId.cc,v 1.21 2007/10/24 14:31:39 fabiocos Exp $
+ * \version $Id: RPCDetId.cc,v 1.23 2009/05/06 22:17:53 mmaggi Exp $
  * \date 02 Aug 2005
  */
 
@@ -115,20 +115,22 @@ RPCDetId::buildfromDB(int region, int ring, int trlayer, int sector,
     if(ring>0) eta_id = 12-trlayer;
     int plane_id = abs(ring);
     int sector_id = sector;
-    // patch to fix phi rotation
-    sector_id--;
-    if (sector_id==0){
-      sector_id=36;
-      if (plane_id > 1 && trlayer == 1 )
-	sector_id=18;
+
+    if (region <0){
+      if (sector_id < 20 ){
+	sector_id = 19+ 1-sector_id;
+      }else{
+	sector_id = 36+20-sector_id;
+      }
     }
+    sector_id-=1;
+
     //
     int copy_id = 1;
     int roll_id = iroll;
     trIndex=(eta_id*10000+plane_id*1000+sector_id*10+copy_id)*10+ roll_id;
   }
   this->buildfromTrIndex(trIndex);
-
 }
 
 void
@@ -164,6 +166,17 @@ RPCDetId::buildfromTrIndex(int trIndex)
   }
   trIndex = trIndex%10000;
   int sector_id = trIndex/100;
+  if (region!=0) {
+    sector_id+=1;
+    if (sector_id==37)sector_id=1;
+  }
+  if (region==-1){
+    if (sector_id < 20 ){
+      sector_id = 19+ 1-sector_id;
+    }else{
+      sector_id = 36+20-sector_id;
+    }
+  }
   trIndex = trIndex%100;
   int copy_id = trIndex/10;
   int sector=(sector_id-1)/3+1;
